225bb9adf1bbeee136bd2cb41c9e0ff56a2912d3
Ever Mamani Vicente <ar2297234@gmail.com>
2022-09-07 08:41:53 +0800

    Grupo 2 - Clase 2 - Implementacion de cactus

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,41 +1,37 @@
-from distutils.command.build import build
 from signal import default_int_handler
 import pygame
 
-from dino_runner.utils.constants import RUNNING 
+from dino_runner.utils.constants import RUNNING
  
 class Dinosaur() :
   X_POS = 80 
   Y_POS = 310 
 
-
   def __init__(self) -> None:
        self.image = RUNNING[0]
        self.dino_rect = self.image.get_rect()
-       self.dino_rect.x = self.X_POS 
-       self.dino_rect.y = self.Y_POS 
+       self.dino_rect.x = self.X_POS
+       self.dino_rect.y = self.Y_POS
        self.step_index = 0
-       self.patita_izquierda = True #implementation dino caminar
-
+       self.feet_movement = True
 
   def update(self):
         pass
 
-
   def draw(self , screen):
-        screen.blit(self.image, ( self.dino_rect.x , self.dino_rect.y )) 
-
+        screen.blit(self.image, ( self.dino_rect.x , self.dino_rect.y ))
 
   def run(self):
-      if self.step_index < 5 :#implementation dino caminar 
-            self.patita_izquierda = not self.patita_izquierda
-            self.step_index = 0
-      if self.patita_izquierda:
-             self.image = RUNNING[0] 
-      else :
-            self.image = RUNNING[1]#implementation dino caminar
+        if self.step_index < 5 :
+          self.feet_movement = not self.feet_movement
+          self.step_index = 0
+        if self.feet_movement:
+          self.image = RUNNING[0]
+        else:
+          self.image = RUNNING[1]
 
-      self.dino_rect = self.image.get_rect()
-      self.dino_rect.x = self.X_POS
-      self.dino_rect.y = self.Y_POS
-      self.step_index +=1 
+        # self.image = RUNNING[0] if self.step_index < 5 else RUNNING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS
+        self.step_index +=1
--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,13 +1,16 @@
 import pygame
+from turtle import Screen
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
-
 from dino_runner.components.dinosaur import Dinosaur
+
+from dino_runner.components.obstaculomanager import ObstacleManager
 
 class Game:
     def __init__(self):
         pygame.init()
         pygame.display.set_caption(TITLE)
         pygame.display.set_icon(ICON)
+        #                                         alto y ancho
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
         self.playing = False
@@ -15,8 +18,8 @@ class Game:
         self.x_pos_bg = 0
         self.y_pos_bg = 380
 
-        self.player = Dinosaur() 
-
+        self.player = Dinosaur()
+        self.obstacle_manager = ObstacleManager()
 
     def run(self):
         self.playing = True
@@ -32,21 +35,23 @@ class Game:
                 self.playing = False
 
     def update(self):
-        self.player.run() #implementation dino caminar
+        self.player.run()
+        self.obstacle_manager.update(self)
 
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
         self.draw_background()
         self.player.draw(self.screen)
+        self.obstacle_manager.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
+
 
     def draw_background(self):
         image_width = BG.get_width()
         self.screen.blit(BG, (self.x_pos_bg, self.y_pos_bg))
         self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
-
         if self.x_pos_bg <= -image_width:
             self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
             self.x_pos_bg = 0
